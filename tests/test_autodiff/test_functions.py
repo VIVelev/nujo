f4cbb933ecb9dd65a548bb324a69d530564afb38
Victor Velev <victorivelev@gmail.com>
2020-03-08 20:10:43 +0200

    [Fix] Types tests

--- tests/test_autodiff/test_functions.py
+++ tests/test_autodiff/test_functions.py
@@ -13,7 +13,7 @@ def test_addition(get_tensors):
     add = funcs.Addition(A, B)
 
     # Test Forwardprop
-    C = add.forward()
+    C = add()
     assert isinstance(C, Tensor)
     assert (A.value + B.value == C.value).all()
 
@@ -21,8 +21,8 @@ def test_addition(get_tensors):
     grad = add.backward()
     assert len(grad) == 2
 
-    assert isinstance(grad[0], ndarray)
-    assert isinstance(grad[1], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
+    assert type(grad[1]) in [ndarray, int, float]
 
     # Test Derivative computation
     assert grad[0] == 1
@@ -38,7 +38,7 @@ def test_negation(get_tensors):
     neg = funcs.Negation(A)
 
     # Test Forwardprop
-    C = neg.forward()
+    C = neg()
     assert isinstance(C, Tensor)
     assert (-A.value == C.value).all()
 
@@ -46,7 +46,7 @@ def test_negation(get_tensors):
     grad = neg.backward()
     assert len(grad) == 1
 
-    assert isinstance(grad[0], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
 
     # Test Derivative computation
     assert grad[0] == -1
@@ -61,7 +61,7 @@ def test_multiplication(get_tensors):
     mul = funcs.Multiplication(A, B)
 
     # Test Forwardprop
-    C = mul.forward()
+    C = mul()
     assert isinstance(C, Tensor)
     assert (A.value * B.value == C.value).all()
 
@@ -69,8 +69,8 @@ def test_multiplication(get_tensors):
     grad = mul.backward()
     assert len(grad) == 2
 
-    assert isinstance(grad[0], ndarray)
-    assert isinstance(grad[1], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
+    assert type(grad[1]) in [ndarray, int, float]
 
     # Test Derivative computation
     assert (grad[0] == B.value).all()
@@ -86,7 +86,7 @@ def test_reciprocal(get_tensors):
     recipr = funcs.Reciprocal(A)
 
     # Test Forwardprop
-    C = recipr.forward()
+    C = recipr()
     assert isinstance(C, Tensor)
     assert (1 / A.value == C.value).all()
 
@@ -94,7 +94,7 @@ def test_reciprocal(get_tensors):
     grad = recipr.backward()
     assert len(grad) == 1
 
-    assert isinstance(grad[0], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
 
     # Test Derivative computation
     assert (grad[0] == -1 / (A.value**2)).all()
@@ -109,7 +109,7 @@ def test_power(get_tensors):
     pow = funcs.Power(A, 2)
 
     # Test Forwardprop
-    C = pow.forward()
+    C = pow()
     assert isinstance(C, Tensor)
     assert (A.value**2 == C.value).all()
 
@@ -117,8 +117,8 @@ def test_power(get_tensors):
     grad = pow.backward()
     assert len(grad) == 2
 
-    assert isinstance(grad[0], ndarray)
-    assert isinstance(grad[1], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
+    assert type(grad[1]) in [ndarray, int, float]
 
     # Test Derivative computation
     assert (grad[0] == 2 * A.value).all()
@@ -134,7 +134,7 @@ def test_MatrixMul(get_tensors):
     matmul = funcs.MatrixMul(A, B)
 
     # Test Forwardprop
-    C = matmul.forward()
+    C = matmul()
     assert isinstance(C, Tensor)
     assert (A.value @ B.value == C.value).all()
 
@@ -142,8 +142,8 @@ def test_MatrixMul(get_tensors):
     grad = matmul.backward()
     assert len(grad) == 2
 
-    assert isinstance(grad[0], ndarray)
-    assert isinstance(grad[1], ndarray)
+    assert type(grad[0]) in [ndarray, int, float]
+    assert type(grad[1]) in [ndarray, int, float]
 
     # Test Derivative computation
     dA, dB = funcs.MatrixMul.differentiate(A, B)
