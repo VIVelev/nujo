d66a64acfa102724d75833d715125eff472dff37
Victor Velev <victorivelev@gmail.com>
2020-03-16 01:43:26 +0200

    [Update] Remove unnecessary param naming in optimizer's step

--- nujo/optim/optimizer.py
+++ nujo/optim/optimizer.py
@@ -30,7 +30,7 @@ class Optimizer:
             for l in range(len(self.params)):
                 # Iterate over params in layer `l`
                 for i in range(len(self.params[l])):
-                    self.params[l][i].name = f'layer[{l}]-param[{i}]'
+                    # self.params[l][i].name = f'layer[{l}]-param[{i}]'
 
                     self.params[l][i] <<= self.update_rule(
                         self.params[l][i], self.params[l][i].grad)
