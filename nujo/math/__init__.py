d709f72c5c0f9eaab5a591e73dfbfe20907b616e
Victor Velev <victorivelev@gmail.com>
2020-03-18 13:55:29 +0200

    [Update] easier to maintain imports

--- nujo/math/__init__.py
+++ nujo/math/__init__.py
@@ -1,12 +1,2 @@
-from nujo.math.aggregate import (max, mean, median, min, prod, stddev, sum,
-                                 variance)
-from nujo.math.scalar import (abs, ceil, exp, floor, log, log2, log10, round,
-                              sqrt)
-
-__all__ = [
-    'max', 'mean', 'median', 'min',
-    'prod', 'stddev', 'sum', 'variance',
-
-    'abs', 'ceil', 'exp', 'floor', 'log',
-    'log2', 'log10', 'round', 'sqrt',
-]
+from nujo.math.aggregate import *
+from nujo.math.scalar import *
