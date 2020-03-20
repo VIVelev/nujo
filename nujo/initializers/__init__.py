f0f5c2f0c81380ea49fba76296592423d3e43d52
Victor Velev <victorivelev@gmail.com>
2020-03-20 09:47:59 +0200

    [up:tensor-initializers] add docstrings to packages math, inititializers __init__

--- nujo/initializers/__init__.py
+++ nujo/initializers/__init__.py
@@ -1,2 +1,6 @@
+''' The functions presented here are just tiny wrappers
+    around `numpy` functions.
+'''
+
 from nujo.initializers.basic import *
 from nujo.initializers.random import *
--- nujo/math/__init__.py
+++ nujo/math/__init__.py
@@ -1,2 +1,6 @@
+''' The functions presented here are just tiny wrappers
+    around `numpy` functions.
+'''
+
 from nujo.math.aggregate import *
 from nujo.math.scalar import *
