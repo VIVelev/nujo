5cbc68921abeefd4aaffe8f8c56c2b7ee48a3cff
Victor Velev <victorivelev@gmail.com>
2020-03-24 10:58:18 +0200

    [up:nujo-flow] computational flow implementation

---
+++ nujo/flow.py
@@ -0,0 +1,69 @@
+from nujo.autodiff.tensor import Tensor
+
+__all__ = [
+    'FlowSetup',
+    'Flow',
+]
+
+
+class FlowSetup(type):
+    def __call__(cls, *args, **kwargs):
+        obj = type.__call__(cls, *args, **kwargs)
+        obj._register_parameters()
+        return obj
+
+
+class Flow(metaclass=FlowSetup):
+    ''' A computational Flow
+
+    Flow of nujo functions.
+
+    Parameters:
+    -----------
+    name : string
+    subflows : list of flows
+
+    '''
+    def __init__(self, name='<Flow>', subflows=[]):
+        self.name = name
+
+        self.subflows = []
+        self.parameters = []
+
+        self.append(*subflows)
+
+    def _register_parameters(self):
+        for prop_name in dir(self):
+            prop = getattr(self, prop_name)
+            if isinstance(prop, Tensor):
+                self.parameters.append(prop)
+
+    def append(self, *flows: 'Flow') -> None:
+        for flow in flows:
+            self.name += ' >> ' + flow.name
+            self.subflows.append(flow)
+
+            if getattr(flow, 'parameters', False):
+                self.parameters.append(flow.parameters)
+
+    def forward(self, x):
+        output_x = x
+        for subflow in self:
+            output_x = subflow.forward(output_x)
+
+        return output_x
+
+    def __call__(self, x):
+        self.forward(x)
+
+    def __rshift__(self, other):
+        return Flow(subflows=[self, other])
+
+    def __getitem__(self, key):
+        return self.subflows[key]
+
+    def __iter__(self):
+        return iter(self.subflows)
+
+    def __repr__(self):
+        return self.name
