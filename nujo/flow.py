6a6eab73b3243374a934de87f97093cb9e05e032
Victor Velev <victorivelev@gmail.com>
2020-04-15 15:37:45 +0300

    [fix:activation-functions] proper flow type hinting

--- nujo/flow.py
+++ nujo/flow.py
@@ -2,6 +2,9 @@
 '''
 
 from copy import deepcopy
+from numbers import Number
+
+from numpy import ndarray
 
 from nujo.autodiff.tensor import Tensor
 
@@ -106,7 +109,7 @@ class Flow(metaclass=_FlowSetup):
 
         return retflow
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> Tensor or ndarray or Number:
         ''' Flow Forward
 
         The flow computation is defined here.
@@ -117,7 +120,7 @@ class Flow(metaclass=_FlowSetup):
 
         Returns:
         --------
-        res : Tensor, computed result
+        res : Tensor or ndarray or Number, computed result
 
         '''
 
@@ -127,7 +130,7 @@ class Flow(metaclass=_FlowSetup):
 
         return output_x
 
-    def copy(self):
+    def copy(self) -> 'Flow':
         ''' Make a copy of the current flow
         '''
 
