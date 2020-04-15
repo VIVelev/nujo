af64d9638d4d21f72cbee3ac8546de07b596b5c4
Victor Velev <victorivelev@gmail.com>
2020-04-15 16:15:56 +0300

    [fix:activation-functions] properly test backward gradient computation of sigmoid and tanh

--- tests/test_nn/test_activations.py
+++ tests/test_nn/test_activations.py
@@ -1,3 +1,5 @@
+from math import e
+
 import pytest
 
 from nujo.autodiff.tensor import Tensor
@@ -5,23 +7,36 @@ from nujo.nn.activations import BinaryStep, Sigmoid, TanH
 
 
 def test_binary_step(input_value):
+    # Test Forward pass
     output = BinaryStep()(input_value)
-    assert isinstance(output, Tensor)
+    assert output == 0
+
+    # Test Backward pass
     output.backward()
+    assert input_value.grad != 0
 
 
 def test_sigmoid(input_value):
+    # Test Forward pass
     output = Sigmoid()(input_value)
-    assert isinstance(output, Tensor)
+    assert output == 1 / (1 + e**-input_value)
+
+    # Test Backward pass
     output.backward()
+    assert input_value.grad == output * (1 - output)
 
 
 def test_tanh(input_value):
+    # Test Forward pass
     output = TanH()(input_value)
-    assert isinstance(output, Tensor)
+    assert output == (e**input_value - e**-input_value) /\
+                     (e**input_value + e**-input_value)
+
+    # Test Backward pass
     output.backward()
+    assert input_value.grad == 1 - output**2
 
 
 @pytest.fixture
 def input_value():
-    return 42
+    return Tensor(0.42, name='test')
