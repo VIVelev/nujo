5916d920a29805484e1c5337f02dcb88a71b08cc
Victor Velev <victorivelev@gmail.com>
2020-05-01 22:28:09 +0300

    [up:typing-fix] properly type hint autodiff node and function

--- nujo/autodiff/_node.py
+++ nujo/autodiff/_node.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from nujo.autodiff._utils import _counter
 
 
@@ -15,14 +17,14 @@ class _Node:
 
     id_generator = _counter()
 
-    def __init__(self, *children, name='Node'):
-        # Parse all children to Tensors
+    def __init__(self, *children: Union[...], name='Node'):
+        # Parse all children that are not Nodes to Tensors
         self.children = []
         for child in children:
             self.add_child(child)
 
         self.name = name
-        self.id = _Node.id_generator.get()
+        self.id: int = _Node.id_generator.get()
 
     def __eq__(self, other):
         return self.id == other.id
