7cb02a3c3ec6acab40c5defed8c4a125c01157f9
Victor Velev <victorivelev@gmail.com>
2020-05-05 00:20:52 +0300

    [up:mnist-example] add graph viz and change optim to Adam

--- examples/mnist_feed_forward_nn.py
+++ examples/mnist_feed_forward_nn.py
@@ -5,6 +5,7 @@ import nujo as nj
 import nujo.nn as nn
 import nujo.objective as obj
 import nujo.optim as optim
+from nujo.utils.viz import ComputationGraphPlotter
 
 net = nn.Linear(28 * 28, 256) >> nn.Sigmoid() \
       >> nn.Linear(256, 128) >> nn.Sigmoid() \
@@ -15,7 +16,7 @@ print(f'Defined net: {net}')
 loss_fn = obj.CrossEntropy()
 print(f'Loss: {loss_fn}')
 
-optimizer = optim.SGD(net.parameters, lr=0.01)
+optimizer = optim.Adam(net.parameters, lr=0.01)
 print(f'Optimizer: {optimizer}')
 
 
@@ -41,6 +42,8 @@ def train(net, x, y, num_epochs):
         # Zero grad
         optimizer.zero_grad()
 
+    return loss
+
 
 if __name__ == '__main__':
     mndata = MNIST('datasets/MNIST', gz=False)
@@ -58,4 +61,8 @@ if __name__ == '__main__':
     images = nj.Tensor(images, name='X_train')
     labels = nj.Tensor(labels.T, name='y_train')
 
-    train(net, images, labels, 1000)
+    loss = train(net, images, labels, 1000)
+
+    # Visualize the Neural Network as a computation graph
+    cg_plot = ComputationGraphPlotter(filename='graph').create(loss)
+    cg_plot.view()
