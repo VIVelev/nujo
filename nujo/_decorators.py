7a7a903a7ce162eb97f5510d41c59e3a0b1990d0
Victor Velev <victorivelev@gmail.com>
2020-05-04 14:24:11 +0300

    [up:nujo-profiler] add decorators for line_profiler and memory_profiler

---
+++ nujo/_decorators.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'decorate_if',
+    'decorate_if_defined',
+]
+
+
+def decorate_if(condition, decorator):
+    return decorator if condition else lambda x: x
+
+
+def decorate_if_defined(decorator):
+    return globals().get(decorator, lambda x: x)
