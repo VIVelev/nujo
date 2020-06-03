9878e3dde0db41c0cc2ff20c513fcdb2d7454463
Victor Velev <victorivelev@gmail.com>
2020-06-03 18:53:35 +0300

    [up:conv-dilation] custom implementation of cached_property to support python 3.7

--- nujo/autodiff/_functions/_transform.py
+++ nujo/autodiff/_functions/_transform.py
@@ -1,4 +1,3 @@
-from functools import cached_property
 from numbers import Number
 from typing import List, Optional, Tuple, Union
 
@@ -13,6 +12,32 @@ __all__ = [
     '_ConstPad',
     '_Im2col',
 ]
+
+
+class cached_property(object):
+    ''' A property that is only computed once per instance and then replaces itself
+    with an ordinary attribute.
+
+    Deleting the attribute resets the property.
+
+    '''
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+
+        key = self.func.__name__
+        cache = obj.__dict__
+
+        if key in cache:
+            return cache[key]
+        else:
+            value = cache[key] = self.func(obj)
+            return value
+
 
 # ====================================================================================================
 
