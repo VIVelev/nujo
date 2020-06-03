2c0bea3af9e862ccf9c8ddfc1ffaf545d0fee170
Victor Velev <victorivelev@gmail.com>
2020-06-03 20:30:40 +0300

    [fix:trigonometric-funcs] methods implementation

--- nujo/autodiff/_functions/_trigonometric.py
+++ nujo/autodiff/_functions/_trigonometric.py
@@ -1,67 +1,44 @@
-from numbers import Number
-from typing import List, Union
-
 from numpy import cos, ndarray, sin, tan
 
 from nujo.autodiff.function import Function
-from nujo.autodiff.tensor import Tensor
 
-__all__ = ['_Sin', '_Cos', '_Tan']
+__all__ = [
+    '_Sin',
+    '_Cos',
+    '_Tan',
+]
 
 # ====================================================================================================
 
 
 class _Sin(Function):
-    ''' Differable sine function
-
-    '''
-    def __init__(self, input: Union[Tensor, ndarray, List[Number], Number]):
-        super(_Sin, self).__init__(input)
-
     def forward(self) -> ndarray:
         return sin(self.children[0].value)
 
     def backward(self, idx: int, accum_grad: Function.T) -> Function.T:
-        # cos(X)
-        return Tensor(cos(self.children[0].value))
+        return accum_grad * cos(self.children[0].value)
 
 
 # ====================================================================================================
 
 
 class _Cos(Function):
-    ''' Differable cosine function
-
-    '''
-    def __init__(self, input: Union[Tensor, ndarray, List[Number], Number]):
-        super(_Cos, self).__init__(input)
-
     def forward(self) -> ndarray:
         return cos(self.children[0].value)
 
     def backward(self, idx: int, accum_grad: Function.T) -> Function.T:
-        # -sin(X)
-        return Tensor(-sin(self.children[0].value))
+        return accum_grad * -sin(self.children[0].value)
 
 
 # ====================================================================================================
 
 
 class _Tan(Function):
-    ''' Differable tangent function
-
-    tan(X) = sin(X) / cos(X)
-
-    '''
-    def __init__(self, input: Union[Tensor, ndarray, List[Number], Number]):
-        super(_Tan, self).__init__(input)
-
     def forward(self) -> ndarray:
         return tan(self.children[0].value)
 
     def backward(self, idx: int, accum_grad: Function.T) -> Function.T:
-        # sec^2(X)
-        return Tensor((1 / cos(self.children[0].value))**2)
+        return accum_grad * (1 / cos(self.children[0].value))**2
 
 
 # ====================================================================================================
