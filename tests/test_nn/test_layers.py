78a36ca3032c0feef000aac00eff49c09a0840b2
Victor Velev <victorivelev@gmail.com>
2020-06-08 16:26:03 +0300

    [fix:conv-test] padding: backward padding trimmming

--- tests/test_nn/test_layers.py
+++ tests/test_nn/test_layers.py
@@ -22,6 +22,39 @@ def test_single_conv2d(image_input):
 
     # Test Backward
     nj_output.backward()
+    nj_params = list(nj_conv.parameters())
+
+    assert nj_params[0].shape == nj_conv[0].b.shape
+    assert nj_params[1].shape == nj_conv[0].kernels.shape
+
+
+# ====================================================================================================
+
+
+def test_chained_conv2d(image_input):
+    nj_tensor, torch_tensor = image_input
+
+    nj_conv = nj_nn.Conv2d(3, 6, 4, stride=2, padding=4, dilation=1) >> \
+        nj_nn.Conv2d(6, 9, 5, stride=2, padding=4, dilation=1) >> \
+        nj_nn.Conv2d(9, 12, 6, stride=2, padding=4, dilation=1)
+
+    torch_conv = torch_nn.Sequential(
+        torch_nn.Conv2d(3, 6, 4, stride=2, padding=4, dilation=2),
+        torch_nn.Conv2d(6, 9, 5, stride=2, padding=4, dilation=2),
+        torch_nn.Conv2d(9, 12, 6, stride=2, padding=4, dilation=2))
+
+    # Test Forward
+    nj_output = nj_conv(nj_tensor)
+    torch_output = torch_conv(torch_tensor)
+
+    assert nj_output.shape == torch_output.shape
+
+    # Test Backward
+    nj_output.backward()
+    nj_params = list(nj_conv.parameters())
+
+    assert nj_params[0].shape == nj_conv[0].b.shape
+    assert nj_params[1].shape == nj_conv[0].kernels.shape
 
 
 # ====================================================================================================
